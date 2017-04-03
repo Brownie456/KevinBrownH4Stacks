c9a89e293e1271042dc92d26b7d8e78da82431f1
Brownie456 <kbrown23@albany.edu>
2017-04-02 23:10:45 -0400

    classes
    added classes code

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Brownie on 4/2/2017.
+ */
+public class Main {
+    public void Main String(args)
+}
---
+++ src/Stack.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Brownie on 4/2/2017.
+ */
+public class Stack {
+
+}
